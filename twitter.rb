24830f96b62a75cade8adf84bf86d92e7da1ce7e
Angela <angelahdezd@gmail.com>
2013-09-29 23:30:59 +0200

    Terminando

--- twitter.rb
+++ twitter.rb
@@ -8,35 +8,39 @@ require 'erb'
 
 class Twitts
 
-	
+	#Inicializar variables
 	def initialize
 		@todo_tweet = []
 		@name = ''
 		@number = 0		
 	end
 
+	#Acceso al HTML para mostrar los resultados
 	def erb(template)
   		template_file = File.open("twitter.html.erb", 'r')
   		ERB.new(File.read(template_file)).result(binding)
 	end
 	
+	#Método call
 	def call env
 	    req = Rack::Request.new(env)
 	    
-	    @todo_tweet = []
 	    binding.pry if ARGV[0]
 	   
+	   #Si no esta vacio , no es un espacio y el usuario existe en Twitter el nombre es el introducido
 	    @name = (req["firstname"] && req["firstname"] != '' && Twitter.user?(req["firstname"]) == true ) ? req["firstname"] : ''
 
 		@number = (req["n"] && req["n"].to_i>1 ) ? req["n"].to_i : 1
 		puts "#{req["n"]}"
 		
+		#Si el nombre existe buscamos sus últimos Tweets
 		if @name == req["firstname"]
 			puts "#{@todo_tweet}"
 			ultimos_t = Twitter.user_timeline(@name,{:count=>@number.to_i})
 			@todo_tweet =(@todo_tweet && @todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''				
 		end
 
+		#Invoca a erb
 		Rack::Response.new(erb('twitter.html.erb'))
 	end
 
